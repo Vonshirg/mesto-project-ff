c46ee1ffa2f9a144f72df8094af002467e32422b
Vonshirg <vonshirg@gmail.com>
2024-02-13 16:38:51 +0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
+Проектная работа Mesto
+https://github.com/Vonshirg/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCard(item, funcDelete) {
+  const placesItem = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = placesItem.querySelector(".card__image");
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  const cardTitle = placesItem.querySelector(".card__title");
+  cardTitle.textContent = item.name;
+  const deleteButton = placesItem.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", (evt) => {
+    deleteCard(evt);
+  });
+  return placesItem;
+}
 
 // @todo: Функция удаления карточки
-
+const deleteCard = (event) => {
+  event.target.closest(".places__item").remove();
+};
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  placesList.append(createCard(item, deleteCard));
+});
+console.log(initialCards.length);
